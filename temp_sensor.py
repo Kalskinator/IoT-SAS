15a089579ab366bf89e4be34d9871d2c4d1b0119
djordjedimitrov <dimitrovdjordje@gmail.com>
2025-04-10 17:49:04 +0200

    Added the config.ini file and updated .gitignore

--- temp_sensor.py
+++ temp_sensor.py
@@ -3,48 +3,49 @@ import json
 import random
 from awscrt import mqtt
 from awsiot import mqtt_connection_builder
+import configparser
 
-ENDPOINT = "a1y7d41s0oj85v-ats.iot.eu-north-1.amazonaws.com"  
-CLIENT_ID = "TemperatureSensor"
-TOPIC = "smart_office_3/temperature"
-CERT_PATH = "certs/device.pem.crt"
-KEY_PATH = "certs/private.pem.key"
-ROOT_CA = "certs/AmazonRootCA1.pem"
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+endpoint = config["aws_iot_temp_sensor"]["endpoint"]
+client_id = config["aws_iot_temp_sensor"]["client_id"]
+topic = config["aws_iot_temp_sensor"]["topic"]
+cert_path = config["aws_iot_temp_sensor"]["cert_path"]
+key_path = config["aws_iot_temp_sensor"]["key_path"]
+root_ca = config["aws_iot_temp_sensor"]["root_ca"]
 
 def gen_temperature():
     return round(random.uniform(15.0, 23.0), 1)
 
+
 def main():
     mqtt_connection = mqtt_connection_builder.mtls_from_path(
-        endpoint=ENDPOINT,
+        endpoint=endpoint,
         port=8883,
-        cert_filepath=CERT_PATH,
-        pri_key_filepath=KEY_PATH,
-        ca_filepath=ROOT_CA,
-        client_id=CLIENT_ID,
+        cert_filepath=cert_path,
+        pri_key_filepath=key_path,
+        ca_filepath=root_ca,
+        client_id=client_id,
         clean_session=False,
         keep_alive_secs=30
     )
 
-    print(f"[Sensor] Connecting to {ENDPOINT}...")
+    print(f"[Sensor] Connecting to {endpoint}...")
     connect_future = mqtt_connection.connect()
     connect_future.result()
     print("[Sensor] Connected!")
 
     for i in range(10):
         temperature = gen_temperature()
-
-        payload = {
-            "temperature": temperature,
-        }
-
+        payload = { "temperature": temperature }
         print(f"[Sensor] Publishing: {payload}")
         mqtt_connection.publish(
-            topic=TOPIC,
+            topic=topic,
             payload=json.dumps(payload),
             qos=mqtt.QoS.AT_LEAST_ONCE
         )
-        time.sleep(2)  
+        time.sleep(2)
 
     print("[Sensor] Done sending data. Disconnecting...")
     disconnect_future = mqtt_connection.disconnect()
